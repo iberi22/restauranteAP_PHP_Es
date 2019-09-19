c1c9462453c08e892e3367dad335b72f8ac8e01a
BeRi <iberi22@gmail.com>
2019-09-18 21:15:46 -0500

    cambios estable#1

--- cafe/admin/orders/pos.php
+++ cafe/admin/orders/pos.php
@@ -5,25 +5,25 @@
      }
 
 		// check_message();
-			
+
     ?>
  <style type="text/css">
  .scrolly {
    /*width: auto;*/
     height:268px;
     /*border: thin solid black;*/
-    overflow-x: hidden; 
+    overflow-x: hidden;
     background-color: #eee;
     /*overflow-y: hidden;*/
-} 
+}
  .scrollorder {
    /*width: auto;*/
     height:450px;
     /*border: thin solid black;*/
-    overflow-x: hidden; 
+    overflow-x: hidden;
     /*overflow-y: hidden;*/
     padding: 0px;
-} 
+}
 .page-header{
     font-size: 25px;
     font-weight: bold;
@@ -41,26 +41,26 @@ input[type="checkbox"]{
 }
 
  </style>
-<form class="form" action="controller.php?action=add" method="POST" target="_blank"> 
+<form class="form" action="controller.php?action=add" method="POST" target="_blank">
 <!-- orders -->
     <div class="col-lg-5">
     	<div class="col-lg-12">
     		<div class="row">
     			<div class="page-header">
-    				List of Orders <a href="addorder.php" class="btn-primary btn btn-s" data-toggle="lightbox" data-title="New Order"><i class="fa fa-plus-circle"></i> New Order</a>
-    			</div> 
+    				Listado de Ordenes <a href="addorder.php" class="btn-primary btn btn-s" data-toggle="lightbox" data-title="New Order"><i class="fa fa-plus-circle"></i> Nueva Orden</a>
+    			</div>
                     <div id="reload" class="scrollorder">
                         <table class="table table-bordered table-hover">
                             <thead>
-                                <tr> 
-                                    <th>Order No.</th> 
-                                    <th>Table No.</th>
-                                    <th>Caterer</th>
+                                <tr>
+                                    <th>Orden No.</th>
+                                    <th>Mesa No.</th>
+                                    <th>Mesar@</th>
                                     <th>Status</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php 
+                                <?php
 
                                    $remarks = isset($_GET['rem']) ? $_GET['rem'] : "" ;
                                     $query = "SELECT * FROM `tblorders` o , `tblusers` u
@@ -68,21 +68,21 @@ input[type="checkbox"]{
                                     $mydb->setQuery($query);
                                     $cur = $mydb->loadResultList();
 
-                                    foreach ($cur as $result) { 
-                                    echo '<tr>'; 
-                                    echo '<td><a style="font-size:15px; font-weight:bold;" href="index.php?view=POS&orderno='.$result->ORDERNO.'&tableno='.$result->TABLENO.'&rem='.$result->REMARKS.'" >'.$result->ORDERNO.'</a></td>'; 
+                                    foreach ($cur as $result) {
+                                    echo '<tr>';
+                                    echo '<td><a style="font-size:15px; font-weight:bold;" href="index.php?view=POS&orderno='.$result->ORDERNO.'&tableno='.$result->TABLENO.'&rem='.$result->REMARKS.'" >'.$result->ORDERNO.'</a></td>';
                                     echo '<td align="center">'.$result->TABLENO.'</td>';
                                     echo '<td>'.$result->FULLNAME.'</td>';
                                     echo '<td>'.$result->REMARKS.'</td>';
                                     echo '</tr>';
-                                 
-                                    } 
-                                ?> 
+
+                                    }
+                                ?>
                             </tbody>
                         </table>
                 </div>
-            </div> 
-    	</div> 
+            </div>
+    	</div>
     </div>
 <!-- end orders -->
  	<!-- SUMARRY -->
@@ -91,7 +91,7 @@ input[type="checkbox"]{
     	<!-- order details -->
     		<div class="row">
     			<div   style="font-size: 24px;font-weight: bold;margin-top: 10px;">
-    				Order Details 
+    				Order Details
     				<small><?php echo isset($_GET['tableno']) ? " for Table Number: ". $_GET['tableno'] : "" ?> <?php echo isset($_GET['rem']) ? "| ". $_GET['rem'] : "" ?></small>
                     <span><?php echo isset($_GET['orderno']) ?  '<a href="addmeal.php?view=addmeal&orderno='.$_GET['orderno'].'&tableno='.$_GET['tableno'].'&rem='.$remarks.'" data-toggle="lightbox" class="btn btn-s btn-primary " data-title="<b>Add Meal</b>"><i class="fa fa-plus-circle"> Add Meal</i></a>' : ''; ?></span>
     				<p style="text-align: right;font-size: 20px;">Order Number:<b style="text-decoration: underline;"> <?php echo isset($_GET['orderno']) ?  $_GET['orderno'] : "NONE" ?></b>
@@ -104,16 +104,16 @@ input[type="checkbox"]{
     			<div class="scrolly">
     			<table id="table" class="table table-hover" style="font-size: 12px" >
     				<thead>
-    					<tr style="font-size: 15px;"> 
+    					<tr style="font-size: 15px;">
     					    <th>Meal</th>
 							<th width="60">Price</th>
 							<th width="50" style="text-align: center;">Qty</th>
 							<th width="90">Amount</th>
 							<th width="30">Action</th>
-    					</tr> 
+    					</tr>
     				</thead>
     				<tbody>
-    					<?php 
+    					<?php
     					$total = 0;
     						if (isset($_GET['orderno'])) {
     							# code...
@@ -123,8 +123,8 @@ input[type="checkbox"]{
 						  		$mydb->setQuery($query);
 						  		$cur = $mydb->loadResultList();
 
-								foreach ($cur as $result) { 
-						  		echo '<tr>'; 
+								foreach ($cur as $result) {
+						  		echo '<tr>';
 						  		echo '<td style="font-size:15px;">'.$result->DESCRIPTION.'</td>';
 						  		echo '<td style="font-size:15px;">&#8369; <input type="hidden" id="'.$result->ORDERID.'orderprice" value="'.$result->PRICE.'" >'.$result->PRICE.'</td>';
 						  		echo '<td style="font-size:15px;"><input type="number" min="1" class="orderqty" data-id="'.$result->ORDERID.'" id="'.$result->ORDERID.'orderqty" value="'.$result->QUANTITY.'" style="width:50px"></td>';
@@ -133,27 +133,27 @@ input[type="checkbox"]{
                                 echo '<td style="text-align:center;"><a title="Cancel Order" class="btn btn-xs btn-danger" style="text-decoration:none;" href="controller.php?action=delete&id='.$result->ORDERID.'&rem='.$result->REMARKS.'"><i style="font-size:15px; padding:2px;" class="fa fa-trash-o"></i></a></td>';
 						  		echo '</tr>';
 
-						  		$total += $result->SUBTOTAL; 
+						  		$total += $result->SUBTOTAL;
 
-						  	 
-						  		} 
+
+						  		}
     						}
-					  		
-				  		?> 
+
+				  		?>
 				  		<!-- <tr>
 				  			<td colspan="4"></td>
 				  		</tr> -->
     				</tbody>
     			</table>
-    				
-    			</div> 
+
+    			</div>
     		<!-- <hr/> -->
     		<!-- end order details -->
-    		<!-- summary --> 
+    		<!-- summary -->
     			<div style="font-size: 19px;font-weight: bold;margin-top:20px;margin-bottom: 3px">
     				Summary
     			</div>
-                
+
     			<table class="table table-bordered">
     				<thead>
     					<tr>
@@ -191,15 +191,15 @@ input[type="checkbox"]{
     				<button target="_blank" type="submit" name="save" class="btn btn-primary btn-lg fa fa-save" id="save"> Save & Print</button>
                     <a target="_blank" href="tempreceipt.php?orderno=<?php echo isset($_GET['orderno']) ?  $_GET['orderno'] : "NONE" ?>&tableno=<?php echo isset($_GET['tableno']) ?  $_GET['tableno'] : "NONE" ?>" class="btn btn-default btn-lg fa fa-print"> <b>Print for Cook</b></a>
     			</div>
-    		</div> 
+    		</div>
     		<!-- end summary -->
-     	</div> 
+     	</div>
     </div>
     </form>
 
   <script>
 function SearchTable() {
-  // Declare variables 
+  // Declare variables
   var input, filter, table, tr, td, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
@@ -216,7 +216,7 @@ function SearchTable() {
       } else {
         tr[i].style.display = "none";
       }
-    } 
+    }
   }
 }
 </script>
